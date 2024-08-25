8a59fd9bdf9f7a1727d1e2eb269268e38be9239b
abhay333d <abhaydandge333@gmail.com>
2024-08-26 00:47:28 +0530

    created a 2d canvas in js and appended it to the html dom. now using the glow.png to draw the cursor tracking, it will show the movement of the cursor on the 2d canvas

--- src/script.js
+++ src/script.js
@@ -74,6 +74,36 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(sizes.pixelRatio);
 
 /**
+ * Dispacement
+ */
+const displacement = {};
+
+//2D canvas
+displacement.canvas = document.createElement("canvas");
+displacement.canvas.width = 128;
+displacement.canvas.height = 128;
+displacement.canvas.style.position = "fixed";
+displacement.canvas.style.width = "512px";
+displacement.canvas.style.height = "512px";
+displacement.canvas.style.top = 0;
+displacement.canvas.style.left = 0;
+displacement.canvas.style.zIndex = 10;
+document.body.append(displacement.canvas);
+
+//Context
+displacement.context = displacement.canvas.getContext("2d");
+displacement.context.fillRect(
+  0,
+  0,
+  displacement.canvas.width,
+  displacement.canvas.height
+);
+
+//Glow image
+displacement.glowImage = new Image();
+displacement.glowImage.src = "./glow.png";
+
+/**
  * Particles
  */
 const particlesGeometry = new THREE.PlaneGeometry(10, 10, 200, 200);
