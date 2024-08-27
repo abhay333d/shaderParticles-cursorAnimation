6fb63a3b85152b9079ccee17d06a3af4f029fe4d
abhay333d <abhaydandge333@gmail.com>
2024-08-28 00:10:57 +0530

    using the displacement canvas to get the texture colors

--- src/script.js
+++ src/script.js
@@ -123,6 +123,9 @@ window.addEventListener("pointermove", (event) => {
   displacement.screenCursor.y = -(event.clientY / sizes.height) * 2 + 1;
 });
 
+//Textures
+displacement.texture = new THREE.CanvasTexture(displacement.canvas);
+
 /**
  * Particles
  */
@@ -139,6 +142,7 @@ const particlesMaterial = new THREE.ShaderMaterial({
       )
     ),
     uPictureTexture: new THREE.Uniform(textureLoader.load("./picture-5.png")),
+    uDisplacementTexture: new THREE.Uniform(displacement.texture),
   },
 });
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
@@ -189,6 +193,9 @@ const tick = () => {
     glowSize,
     glowSize
   );
+
+  //Texture
+  displacement.texture.needsUpdate = true;
 
   // Render
   renderer.render(scene, camera);
--- src/shaders/particles/vertex.glsl
+++ src/shaders/particles/vertex.glsl
@@ -1,5 +1,6 @@
 uniform vec2 uResolution;
 uniform sampler2D uPictureTexture;
+uniform sampler2D uDisplacemnetTexture;
 
 varying vec3 vColor; 
 
