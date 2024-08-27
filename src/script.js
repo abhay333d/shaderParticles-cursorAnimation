73963a7cd447fb4baa8245628b7f8b6c14663ed3
abhay333d <abhaydandge333@gmail.com>
2024-08-28 00:50:54 +0530

    now it works on the backside also

--- src/script.js
+++ src/script.js
@@ -106,7 +106,7 @@ displacement.glowImage.src = "./glow.png";
 //Interactive Plane
 displacement.interactivePlane = new THREE.Mesh(
   new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshBasicMaterial({ color: "red" })
+  new THREE.MeshBasicMaterial({ side: THREE.DoubleSide })
 );
 displacement.interactivePlane.visible = false;
 scene.add(displacement.interactivePlane);
